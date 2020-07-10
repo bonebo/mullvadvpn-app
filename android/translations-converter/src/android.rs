a0dd0f2e323f84b2d1618e87add70372e72aa239
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-07-10 18:15:13 +0000

    Collect known Android strings

--- android/translations-converter/src/android.rs
+++ android/translations-converter/src/android.rs
@@ -8,7 +8,16 @@ pub struct StringResources {
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct StringResource {
-    name: String,
+    pub name: String,
     #[serde(rename = "$value")]
-    value: String,
+    pub value: String,
+}
+
+impl IntoIterator for StringResources {
+    type Item = StringResource;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.entries.into_iter()
+    }
 }
