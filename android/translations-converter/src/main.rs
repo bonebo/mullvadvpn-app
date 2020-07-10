22ac93e06de996e613a57ae9b7c5470bec9b990f
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-07-10 21:28:15 +0000

    Write generated translation files

--- android/translations-converter/src/main.rs
+++ android/translations-converter/src/main.rs
@@ -5,10 +5,12 @@ use regex::Regex;
 use std::{
     collections::HashMap,
     fs::{self, File},
+    path::Path,
 };
 
 fn main() {
-    let strings_file = File::open("../src/main/res/values/strings.xml")
+    let resources_dir = Path::new("../src/main/res");
+    let strings_file = File::open(resources_dir.join("values/strings.xml"))
         .expect("Failed to open string resources file");
     let string_resources: android::StringResources =
         serde_xml_rs::from_reader(strings_file).expect("Failed to read string resources file");
@@ -33,13 +35,45 @@ fn main() {
         .filter(|file_path| file_path.exists());
 
     for locale_file in locale_files {
-        generate_translations(&known_strings, gettext::load_file(&locale_file));
+        let locale = locale_file
+            .parent()
+            .unwrap()
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap();
+        let destination_dir = resources_dir.join(&android_locale_directory(locale));
+
+        if !destination_dir.exists() {
+            fs::create_dir(&destination_dir).expect("Failed to create Android locale directory");
+        }
+
+        generate_translations(
+            &known_strings,
+            gettext::load_file(&locale_file),
+            destination_dir.join("strings.xml"),
+        );
     }
+}
+
+fn android_locale_directory(locale: &str) -> String {
+    let mut directory = String::from("values-");
+    let mut parts = locale.split("-");
+
+    directory.push_str(parts.next().unwrap());
+
+    if let Some(region) = parts.next() {
+        directory.push_str("-r");
+        directory.push_str(region);
+    }
+
+    directory
 }
 
 fn generate_translations(
     known_strings: &HashMap<String, String>,
     translations: Vec<gettext::MsgEntry>,
+    output_path: impl AsRef<Path>,
 ) {
     let mut localized_resource = android::StringResources::new();
 
@@ -52,5 +86,6 @@ fn generate_translations(
         }
     }
 
-    dbg!(localized_resource);
+    fs::write(output_path, localized_resource.to_string())
+        .expect("Failed to create Android locale file");
 }
